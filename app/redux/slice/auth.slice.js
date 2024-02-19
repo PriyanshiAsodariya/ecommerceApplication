b45226781510abf624a79c763bc7fdb3802657f3
priyanshi <asodariyapriyanshi@gmail.com>
2024-02-19 12:13:29 +0530

    done

--- app/redux/slice/auth.slice.js
+++ app/redux/slice/auth.slice.js
@@ -122,33 +122,7 @@ export const siginFacebook = createAsyncThunk(
 );
 
 
-// export const siginFacebook = createAsyncThunk(
-//     'auth/siginFacebook',
-//     async () => {
-//         // Attempt login with permissions
-//         const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
 
-//         if (result.isCancelled) {
-//             console.log( "Login cancelled" );
-//             throw 'User cancelled the login process';
-//         }else{
-//             result.grantedPermissions.toString();
-//         }
-
-//         // Once signed in, get the users AccessToken
-//         const data = await AccessToken.getCurrentAccessToken();
-
-//         if (!data) {
-//             throw 'Something went wrong obtaining access token';
-//         }
-
-//         // Create a Firebase credential with the AccessToken
-//         const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
-
-//         // Sign-in the user with the credential
-//         return auth().signInWithCredential(facebookCredential);
-//     }
-// )
 
 
 export const authslice = createSlice({
